b666580634a05fc8559856f9b5ba735f076fd2e6
Robin Suter <suter.robin@gmail.com>
2017-09-23 13:42:18 +0200

    try with texlive-latex-extra

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends \
-    texlive-xetex texlive-publishers texlive-lang-german \
+    texlive-xetex texlive-latex-extra \
+    texlive-publishers texlive-lang-german \
     lmodern fonts-sil-gentium-basic \
-    python-pygments xzdec
-
-#install additional LaTeX dependencies
-tlmgr init-usertree
-tlmgr install placeins
+    python-pygments
