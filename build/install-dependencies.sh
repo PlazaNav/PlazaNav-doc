dc92f387424e6d7e40d2fc5165b1765f5056c1de
Robin Suter <suter.robin@gmail.com>
2017-09-23 13:59:49 +0200

    fix dependency name

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends \
-    texlive-xetex texlive-latex-extra texlive-xcolor\
+    texlive-xetex texlive-latex-extra latex-xcolor\
     texlive-publishers texlive-lang-german \
     texlive-fonts-recommended lmodern fonts-sil-gentium-basic \
     python-pygments
