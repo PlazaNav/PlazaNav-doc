e391ad5b011e320b60a3ab031708d5ef8fbc61f3
Robin Suter <suter.robin@gmail.com>
2017-09-23 13:52:21 +0200

    install additional fonts

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -3,5 +3,5 @@ sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends \
     texlive-xetex texlive-latex-extra \
     texlive-publishers texlive-lang-german \
-    lmodern fonts-sil-gentium-basic \
+    texlive-fonts-recommended lmodern fonts-sil-gentium-basic \
     python-pygments
