595487e51bdeab47441aa853223b376ad29bb621
Robin Suter <suter.robin@gmail.com>
2017-09-23 13:36:55 +0200

    initialize tlmgr

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -3,7 +3,8 @@ sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends \
     texlive-xetex texlive-publishers texlive-lang-german \
     lmodern fonts-sil-gentium-basic \
-    python-pygments
+    python-pygments xzdec
 
 #install additional LaTeX dependencies
-sudo tlmgr install placeins
+tlmgr init-usertree
+tlmgr install placeins
