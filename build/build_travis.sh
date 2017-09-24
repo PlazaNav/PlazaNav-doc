f8b6f0db57e7f697a60cebf1b61656c0cd63c5a1
Robin Suter <suter.robin@gmail.com>
2017-09-24 14:40:43 +0200

    upload built pdfs to jira

--- build/build_travis.sh
+++ build/build_travis.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 DATE=`date +"%y%m%d"`
 JOBNAME="PlazaNav-$DATE"
+LATEXCMD="xelatex -shell-escape -jobname=$JOBNAME  -interaction=nonstopmode"
 # Shell escape is needed for pygments syntax highlighting
-xelatex -shell-escape -jobname=$JOBNAME  -interaction=nonstopmode index.tex
+$LATEXCMD index.tex
 # build bibliography
 bibtex $JOBNAME.aux
 # build again with bibliography
-xelatex -shell-escape -jobname=$JOBNAME index.tex
+$LATEXCMD index.tex
 # build a third time to fix potential numbering changes
-xelatex -shell-escape -jobname=$JOBNAME index.tex
+$LATEXCMD index.tex
