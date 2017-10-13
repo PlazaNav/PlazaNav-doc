80a86d482fbc53690fd44e41d2f83476380e9243
Robin Suter <suter.robin@gmail.com>
2017-10-13 20:21:10 +0200

    add more latex dependencies

--- build/texlive-install.sh
+++ build/texlive-install.sh
@@ -17,3 +17,7 @@ c
 r
 i
 " | ./install-tl
+
+# install dependencies
+export PATH="$PATH:$HOME/texlive/bin/x86_64-linux"
+tlmgr install IEEEtran placeins framed threeparttable acronym minted ifplatform bigfoot xstring
