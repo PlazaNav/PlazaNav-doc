10ec30d79559d33f219f0da3626458b873caab62
Robin Suter <suter.robin@gmail.com>
2017-10-13 19:12:30 +0200

    try travis with manual latex installation

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends \
-    texlive-xetex texlive-latex-extra latex-xcolor\
-    texlive-publishers texlive-lang-german \
-    texlive-fonts-recommended lmodern fonts-sil-gentium-basic
+      lmodern fonts-sil-gentium-basic
+#     texlive-xetex texlive-latex-extra latex-xcolor\
+#     texlive-publishers texlive-lang-german \
+#     texlive-fonts-recommended lmodern fonts-sil-gentium-basic
