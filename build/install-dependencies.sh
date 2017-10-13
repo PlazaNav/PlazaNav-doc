662fb8aac4a1b821767ae3332fba06795ecdc59a
Robin Suter <suter.robin@gmail.com>
2017-10-13 19:25:24 +0200

    travis script cleanup

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -2,6 +2,3 @@
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends \
       lmodern fonts-sil-gentium-basic
-#     texlive-xetex texlive-latex-extra latex-xcolor\
-#     texlive-publishers texlive-lang-german \
-#     texlive-fonts-recommended lmodern fonts-sil-gentium-basic
--- build/texlive-install.sh
+++ build/texlive-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# source: https://gist.github.com/kokx/fd218f5c17cf795d6b8b
 # This file will install texlive in a travis worker that does not allow sudo.
 # Hopefully this will become obsolete in the near future.
 mkdir ~/texlive
