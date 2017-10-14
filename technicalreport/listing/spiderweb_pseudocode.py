5a65a56922a4d57fc2dc164a811752b583864e05
Jonas Matter <jonas.matter@bluewin.ch>
2017-10-14 10:17:47 +0200

    update pseudocode

--- technicalreport/listing/spiderweb_pseudocode.py
+++ technicalreport/listing/spiderweb_pseudocode.py
@@ -1,2 +1,9 @@
-def create_spiderwebgraph:
-    do_something()
+def create_spiderweb_graph(plaza_list, line_layer):
+    for plaza in plaza_list:
+        spiderweb = draw_spiderweb(plaza)
+        intersecting_features = get_intersecting_features(plaza, line_layer)
+        entry_points = get_entry_points(plaza, line_layer)
+        connect_entry_points_with_spiderweb(entry_points, spiderweb)
+        # TODO hier Schritte wie bei Visibility Graph um Hindernisse zu entfernen
+        shortest_path(spiderweb)
+        remove_unnecessary_lines(spiderweb)
