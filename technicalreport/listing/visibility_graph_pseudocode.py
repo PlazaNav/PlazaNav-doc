b3852b729c2842cc2c3925ac5f9c9057da15ae25
Robin Suter <suter.robin@gmail.com>
2017-10-14 14:54:50 +0200

    first draft chapter visibility graph

---
+++ technicalreport/listing/visibility_graph_pseudocode.py
@@ -0,0 +1,9 @@
+def optimized_visibility_graph():
+    # TODO: Anderer Typ von Pseudocode??
+    visibility_graph = []
+    for edge in all_edges:
+        if edge_does_not_collide(edge):
+            visibility_graph += edge
+    for point in entry_points:
+        paths = shortest_path_to_all(visibility_graph, entry_points)
+        add_to_routing_graph(paths)
