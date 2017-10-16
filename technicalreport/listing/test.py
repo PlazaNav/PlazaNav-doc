a7106625ec6a0d680d10eb04f192ea8ba5f7dabb
Robin Suter <suter.robin@gmail.com>
2017-10-16 13:47:15 +0200

    fix typos & cleanup

--- technicalreport/listing/test.py
+++
@@ -1,9 +0,0 @@
-import os
-def ask_question(self, question):
-    while True:
-        choice = input("%s (y/N)" % question)
-        choice = choice.lower()
-        if choice == '' or choice == 'n':
-            return False
-        elif choice == 'y':
-            return True
